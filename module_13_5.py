d65c32de884000c83874ef91faf90ff0733824db
santfan <santfan@mail.ru>
2025-01-03 19:08:00 +0300

    Меньше текста. Больше кликов

---
+++ module_13_5.py
@@ -0,0 +1,55 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = '7680362534:AAFxREcENIpGw2eLIBR25rgd7xjTql0mjyQ'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+calc = KeyboardButton('Рассчитать')
+info = KeyboardButton('Информация')
+
+kb.add(calc)
+kb.add(info)
+
+class UserState(StatesGroup):
+    age = State()
+    growth =State()
+    weight =State()
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твокму здоровью', reply_markup= kb)
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите ваш возраст')
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите ваш рост')
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.weight)
+async def calc_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = 10 * float(data['weight']) + 6.25 * float(data['growth']) + 10 * float(data['age']) + 5
+    await message.answer(f'Ваша норма калорий {result} в день')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
